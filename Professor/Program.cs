babacc5bf3ed5105c78f1e756ffc6f570d49bfda
Badr Elbadry <badr.ahmed.2015@gmail.com>
2025-08-26 18:15:49 +0300

    Add project files.

---
+++ Professor/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.OpenApi.Models;
+
+
+namespace Professor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            // Register Swagger services
+            builder.Services.AddEndpointsApiExplorer();
+             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            //// Enable Swagger in development
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI(c =>
+            //    {
+            //        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+            //    });
+            //}
+
+
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
